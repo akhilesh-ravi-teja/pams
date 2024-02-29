6f253e588c01f0bc2672baa92c16a02fe9e49834
Akhilesh Kumar <akhileshkumar@learning-spiral.com>
2024-02-29 17:17:30 +0530

    added

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 $array = array(3, 5, 2, 9);
 $duplicates = array_duplicates($array);
 var_dump($duplicates);
-
+$newArra = [];
 function array_duplicates(array $array)
 {
     return array_diff_assoc($array, array_unique($array));
